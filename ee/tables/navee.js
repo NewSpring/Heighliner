ba71be6ec80f63fd6733e1bf417aac89cda7ba47
James Baxley <james.baxley@newspring.cc>
2016-02-27 20:46:59 -0500

    added parsing of pages uri and full nav of two levels complete

--- ee/tables/navee.js
+++ ee/tables/navee.js
@@ -1,5 +1,6 @@
 
 import _ from "lodash"
+import { unserialize } from "php-unserialize"
 
 export default function(doc){
 
@@ -10,6 +11,11 @@ export default function(doc){
   let orphans = []
 
   for (let row of doc) {
+    if (row.site_pages) {
+      let data = unserialize(new Buffer(row.site_pages, "base64"))
+      row.link = data[row.site_id].uris[row.entry_id]
+    }
+
     if (row.parent === 0) {
       parents[row.id] = row
       continue
--- ee/tables/navee.sql
+++ ee/tables/navee.sql
@@ -4,12 +4,22 @@ SELECT
   s.link,
   s.sort,
   s.navee_id as id,
-  s.rel as image
+  s.rel as image,
+  s.type,
+  s.entry_id,
+  t.site_id,
+  p.site_pages
 FROM
   exp_navee_navs as d
 LEFT JOIN
   exp_navee as s
     ON d.navigation_id = s.navigation_id
+LEFT JOIN
+	exp_channel_titles as t
+		ON s.entry_id = t.entry_id
+LEFT JOIN
+	exp_sites as p
+		ON t.site_id = p.site_id
 WHERE
   d.nav_title=${nav_title}
 ORDER BY s.parent
