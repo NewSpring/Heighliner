03f267d9d231f6d2757c8a28f0469cf3fdf652fd
James Baxley <james.baxley@newspring.cc>
2016-03-07 18:37:29 -0500

    force auth of app and ssl

--- server.js
+++ server.js
@@ -6,11 +6,33 @@ import Schema from "./schemas"
 import bodyParser from "body-parser"
 import forceSSL from "express-force-ssl"
 import morgan from "morgan"
+import auth from "basic-auth"
 
 // let PORT = process.env.DOCKER_HOST ? 80 : 8080
 let PORT = process.env.PORT || 80
 
 const app = express();
+
+
+if (process.env.NODE_ENV === "production") {
+  // force ssl
+  app.use(forceSSL)
+
+  // initial simple auth using Rock creds
+  app.use((req, res, next) => {
+    let creds = auth(req)
+
+    if (!creds || creds.name != "apollos" || creds.pass !=  process.env.ROCK_TOKEN) {
+      res.statusCode = 401
+      res.setHeader('WWW-Authenticate', 'Basic realm="example"')
+      res.end('Access denied')
+      return
+    }
+
+    next()
+  })
+}
+
 
 app.use(morgan("combined"))
 
@@ -22,9 +44,7 @@ app.use(bodyParser.json())
 
 
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(forceSSL)
-// }
+
 
 // Add headers
 app.use((req, res, next) => {
