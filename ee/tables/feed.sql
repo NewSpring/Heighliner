32602727360462eb28c1b8ace7ebf0194ac96a62
John Pinkerton <jpinkerton88@gmail.com>
2016-04-14 11:29:00 -0400

    more home feed fields for sql

--- ee/tables/feed.sql
+++ ee/tables/feed.sql
@@ -1,8 +1,14 @@
 SELECT
   d.entry_id,
+  d.site_id,
+  d.channel_id,
   c.channel_name,
   t.title,
-  t.entry_date
+  t.entry_date,
+  t.status,
+  d.field_id_486 as primary_accent_color,
+  p.child_entry_id as series_id,
+  d.field_id_249 as album_image
 FROM
   exp_channel_data as d
 LEFT JOIN
@@ -11,6 +17,9 @@ LEFT JOIN
 LEFT JOIN
   exp_channel_titles AS t
     ON d.entry_id = t.entry_id
+LEFT JOIN
+  exp_playa_relationships as p
+    ON d.entry_id = p.parent_entry_id
 WHERE
   -- series, sermons, stories, devotionals, articles, music
   d.channel_id IN (4,3,5,27,30,47)
