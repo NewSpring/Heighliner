f427fba2048214a19cd282076a811d67a195c3ae
John Pinkerton <jpinkerton88@gmail.com>
2016-04-24 15:50:23 -0400

    add user lookup and context from token

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ import Schema from "./schemas"
 import bodyParser from "body-parser"
 import forceSSL from "express-force-ssl"
 import morgan from "morgan"
-import auth from "basic-auth"
 import cors from "cors"
+import crypto from "crypto"
+import { Users } from "./apollos"
 
 // let PORT = process.env.DOCKER_HOST ? 80 : 8080
 let PORT = process.env.PORT || 80
@@ -24,23 +25,6 @@ app.get("/alive", (req, res) => {
 if (process.env.NODE_ENV === "production") {
   // force ssl
   // app.use(forceSSL)
-
-  // initial simple auth using Rock creds
-  app.use((req, res, next) => {
-    let creds = auth(req)
-
-    if (!creds || creds.name != "apollos" || creds.pass !=  process.env.ROCK_TOKEN) {
-      res.statusCode = 401
-      res.setHeader('WWW-Authenticate', 'Basic realm="example"')
-      res.end('Access denied')
-      return
-    }
-
-    next()
-  })
-  
-  app.use(morgan("combined"))
-
 }
 
 
@@ -60,10 +44,24 @@ app.use(cors(corsOptions))
 
 
 app.use("/", apolloServer(() => {
+  let user = null;
+
+  if (request.headers.authorization) {
+    let hashedToken = crypto.createHash('sha256');
+    hashedToken.update(request.headers.authorization);
+    hashedToken = hashedToken.digest('base64');
+
+    user = await Users.findOne({
+      "services.resume.loginTokens.hashedToken": hashedToken,
+    }, "_id, services.rock.PrimaryAliasId");
+  }
 
   let graphql = {
     schema: Schema,
     graphiql: process.env.NODE_ENV != "production"
+    context: {
+      user,
+    }
   }
 
   if (process.env.TRACER_APP_KEY) {
