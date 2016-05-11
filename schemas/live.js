3616fac815d36dbcd6177faf53240771ebbc9bf8
James Baxley <james.baxley@newspring.cc>
2016-05-10 22:11:15 -0400

    move some stuff around

--- schemas/live.js
+++ schemas/live.js
@@ -13,7 +13,7 @@ import {
 } from "graphql"
 
 import Promise from "bluebird"
-import MySQL from "../ee/mysql"
+import { getLiveFeed } from "../ee/mysql"
 import ContentType from "./shared/EE/content"
 
 const MediaType = new GraphQLObjectType({
@@ -63,64 +63,8 @@ export default {
     },
   },
   resolve: (_, { site }) => {
-    const liveQuery = Path.join(__dirname, "../ee/util/live.sql")
-    return MySQL(liveQuery);
+
+    return getLiveFeed(site)
+
   }
 }
-
-
-// import { load } from "../util/cache"
-//
-// import { Likes, Users } from "../apollos"
-// import { People, api, parseEndpoint } from "../rock"
-//
-// import { PersonType } from "./shared/rock/person"
-//
-// export default {
-//   type: PersonType,
-//   // description: "A person record in Rock",
-//   args: {
-//     id: {
-//       type: GraphQLInt
-//     },
-//     mongoId: {
-//       type: GraphQLString
-//     },
-//     ttl: {
-//       type: GraphQLInt
-//     },
-//     cache: {
-//       type: GraphQLBoolean,
-//       defaultValue: true
-//     },
-//   },
-//   resolve: (_, { mongoId, id, ttl, cache }) => {
-//
-//     if (!mongoId && !id) {
-//       throw new Error("An id is required for person lookup")
-//     }
-//
-//     if (id) {
-//       return People.get(id, ttl, cache)
-//         .then((people) => (people[0]))
-//
-//     } else if (mongoId) {
-//
-//       return load(
-//         JSON.stringify({"user-_id": mongoId }),
-//         () => (Users.findOne({"_id": mongoId }, "services.rock.PersonId"))
-//       , ttl, cache)
-//         .then((user) => {
-//
-//           if (user && user.services.rock) {
-//             return People.get(user.services.rock.PersonId, ttl, cache)
-//           }
-//
-//           return [{}]
-//         })
-//         .then((people) => (people[0]))
-//     }
-//
-//
-//   }
-// }
