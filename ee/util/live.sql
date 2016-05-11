0d621a2359ff976b8d0a2a23f0bbe55ead1cc90d
benwiley <ben.wiley@newspring.cc>
2016-05-11 11:02:47 -0400

    Use site param and get back service viewing window

--- ee/util/live.sql
+++ ee/util/live.sql
@@ -1,18 +1,20 @@
 SELECT
-  t.title AS channel_title,
-  d.field_id_1128 AS data_matrix,
-  t.status,
-  t.entry_date,
-  t.expiration_date,
-  t.entry_id
+  m.col_id_365 AS serviceType,
+  m.col_id_367 AS startTime,
+  m.col_id_368 AS endTime,
+  m.col_id_366 AS dayOfWeek
 FROM
-  exp_channel_data d
-  JOIN exp_channel_titles t
-    ON t.channel_id = d.channel_id AND t.entry_id = d.entry_id
+  exp_sites s
+  JOIN exp_channel_data d ON d.site_id = s.site_id
+  JOIN exp_channel_titles t ON t.channel_id = d.channel_id AND t.entry_id = d.entry_id AND t.site_id = s.site_id
+  JOIN exp_matrix_data m ON m.entry_id = d.entry_id AND m.site_id = s.site_id
 WHERE
-  d.channel_id = 175
+  s.site_name = '${site_name}'
+  AND d.channel_id = 175
+  AND d.entry_id = 128506
   AND t.entry_date <= UNIX_TIMESTAMP()
   AND (
-  	t.expiration_date = 0
-  	OR t.expiration_date >= UNIX_TIMESTAMP()
-  );
+    t.expiration_date = 0
+    OR t.expiration_date >= UNIX_TIMESTAMP()
+  )
+  AND m.col_id_366 IS NOT NULL;
