fd544b5e6ff8cd80320c270ad9e78c3b3a1b74ac
James Baxley <james.baxley@newspring.cc>
2016-05-22 19:21:10 -0400

    remove connectors from contet

--- src/apollos/users/index.js
+++ src/apollos/users/index.js
@@ -1,12 +1,12 @@
 import { gql } from "../../util"
 const schema = gql("./schema");
 import resolver from "./resolver";
-import connector from "./connector";
+import model from "./model";
 import mocks from "./mocks";
 
 export {
   schema,
   resolver,
-  connector,
+  model,
   mocks,
 }
