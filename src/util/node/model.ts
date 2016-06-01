02cc36a581d189496afca681c3f72c987cb2dfb0
James Baxley <james.baxley@newspring.cc>
2016-06-01 10:12:50 -0400

    using cipher for ids and fix coverage

--- src/util/node/model.ts
+++ src/util/node/model.ts
@@ -1,4 +1,8 @@
 
+declare function require(name:string);
+const Crypto = require("crypto");
+const secret = process.env.SECRET || "LZEVhlgzFZKClu1r";
+
 export default class Node {
   private models: Object[]
 
@@ -19,10 +23,21 @@ export default class Node {
 }
 
 export function createGlobalId(id: string, type: string): string {
-  return new Buffer(`${type}:${id}`).toString("base64");
+  const cipher = Crypto.createCipher("aes192", secret);
+
+  let encrypted = cipher.update(`${type}:${id}`, "utf8", "base64");
+  encrypted += cipher.final("base64");
+
+  return encrypted;
 }
 
 export function parseGlobalId(encodedId: string): { id: string, __type: string } {
-  const [ __type, id ] = new Buffer(encodedId, "base64").toString().split(":");
+
+  const decipher = Crypto.createDecipher("aes192", secret);
+
+  let decrypted = decipher.update(encodedId, "base64", "utf8");
+  decrypted += decipher.final("utf8");
+
+  const [ __type, id ] = decrypted.toString().split(":");
   return { __type, id };
 }
