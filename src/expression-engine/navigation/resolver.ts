755b0a47a452fdb6fed057890c7eec362d403634
James Baxley <james.baxley@newspring.cc>
2016-06-06 13:08:06 -0400

    add todo for children on node interface

--- src/expression-engine/navigation/resolver.ts
+++ src/expression-engine/navigation/resolver.ts
@@ -11,7 +11,15 @@ export default {
     absoluteLink: ({ link, url }) => `${url}${link.substring(1, link.length)}`,
     sort: ({ sort }) => sort,
     image: ({ image }) => image,
-    children: ({ children }) => children,
+    children: ({ children, id }, _, { models }) => {
+      if (children.length) {
+        return children;
+      }
+      
+      // XXX hookup up find by parent method
+      return [];
+      // return models.Navigation.findByParent(id);
+    },
   },
 
 }
