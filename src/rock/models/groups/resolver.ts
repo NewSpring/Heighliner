899c7f2f22cdba58e32ade98f9a527a068dab283
James Baxley <james.baxley@newspring.cc>
2016-06-16 00:36:39 -0400

    feat: WIP on new group search, roughly working tags JAMES-2

--- src/rock/models/groups/resolver.ts
+++ src/rock/models/groups/resolver.ts
@@ -4,11 +4,22 @@ import { createGlobalId } from "../../../util";
 export default {
 
   Query: {
-    groups: (_, { name, id }, { models }) => models.Group.find({ Id: id, Name: name }),
+    groups: (_, { name, id, attributes }, { models }) => {
+      if (attributes) return models.Group.findByAttributes(attributes);
+
+      return models.Group.find({ Id: id, Name: name });
+    },
   },
 
   Group: {
     id: ({ Id }: any, _, $, { parentType }) => createGlobalId(Id, parentType.name),
+    name: ({ Name }) => Name,
+    description: ({ Description }) => Description,
+    active: ({ IsActive }) => IsActive,
+    photo: (data) => {
+      // console.log(data.AttributeValues)
+      return "XXX";
+    },
   },
 
 };
