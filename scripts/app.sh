e174b0df087320da66c4ecbf8dbd41fc05772ad1
James Baxley <james.baxley@newspring.cc>
2016-07-03 13:16:48 -0400

    change key name

--- scripts/app.sh
+++ scripts/app.sh
@@ -44,7 +44,7 @@ yecho "Release Notes:
 $GIT_HISTORY"
 
 yecho "### Configuring aws tool ###"
-aws configure set default.aws_access_key_id $AWS_ACCESS_KEY
+aws configure set default.aws_access_key_id $AWS_ACCESS_KEY_ID
 aws configure set default.aws_secret_access_key $AWS_SECRET_ACCESS_KEY
 aws configure set default.region us-east-1
 
