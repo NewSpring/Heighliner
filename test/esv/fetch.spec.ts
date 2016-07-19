6888ab36c2872f23fc5b36db2499e7dda10fe4f5
John Pinkerton <jpinkerton88@gmail.com>
2016-07-19 13:05:14 -0400

    add some test for scriptures

---
+++ test/esv/fetch.spec.ts
@@ -0,0 +1,21 @@
+import test from "ava";
+import casual from "casual";
+
+import { ESVFetchConnector, connect } from "../../src/esv/fetch";
+
+test("`connect` should fail without any env vars", async (t) => {
+  const ESV = await connect();
+  t.falsy(ESV);
+});
+
+test("`connect` should be fine with esv key", async (t) => {
+  process.env.ESV_KEY = casual.word;
+
+  const ESV = await connect();
+  t.truthy(ESV);
+});
+
+test("`ESVFetchConnector` should export getFromAPI function", t => {
+  const testFetcher = new ESVFetchConnector();
+  t.truthy(testFetcher.getFromAPI);
+});
