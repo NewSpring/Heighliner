6052b273ac960b7b00598b7a1441d09e8590153f
John Pinkerton <jpinkerton88@gmail.com>
2016-07-19 12:25:42 -0400

    add caching for scripture

--- src/esv/models/scripture/model.ts
+++ src/esv/models/scripture/model.ts
@@ -1,8 +1,21 @@
-
 import { ESVFetchConnector } from "../../fetch";
 
+import { Cache, defaultCache } from "../../../util/cache";
+
 class ESV extends ESVFetchConnector {
-  // ESV only does one time
+  public __type: string = "ESV";
+  public cache: Cache;
+
+  constructor({ cache } = { cache: defaultCache }) {
+    super();
+    this.cache = cache;
+  }
+
+  public async get(query: string): Promise<any> {
+    return await this.cache.get(`${this.__type}:${query}`, () => (
+      this.getFromAPI(query)
+    ));
+  }
 };
 
 export default {
