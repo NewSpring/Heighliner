a9987c546111cf5868346bf1319ceebc35092070
delianides <github@delianides.com>
2016-07-30 16:32:32 -0400

    shell for finance tests

---
+++ test/rock/models/finances/resolver.spec.ts
@@ -0,0 +1,38 @@
+import test from "ava";
+// import Resolver from "../../../../src/rock/models/campuses/resolver";
+
+// const sampleData = {
+//   transaction: {
+//     Id: "123ABC",
+//     Amount: "25.00",
+//     AccountId: "12345",
+//   },
+// };
+
+test("`TransactionDetail` has amount and account.", t => {
+  t.pass();
+});
+
+test("`ScheduledTransaction` has start and end date.", t => {
+  t.pass();
+});
+
+test("`ScheduledTransaction` has required meta data.", t => {
+  t.pass();
+});
+
+test("`Transaction` has payment details.", t => {
+  t.pass();
+});
+
+test("`FinancialAccount` is a valid account for contributions.", t => {
+  t.pass();
+});
+
+test("`PaymentDetail` has detailed information about a single payment.", t => {
+  t.pass();
+});
+
+test("`SavedPayment` has the required information to repeat transactions.", t => {
+  t.pass();
+});
