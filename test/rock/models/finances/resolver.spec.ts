c2a2076ee60dabbb86376f68eb7ef838bad062be
delianides <github@delianides.com>
2016-08-02 14:10:08 -0400

    finance plans

--- test/rock/models/finances/resolver.spec.ts
+++ test/rock/models/finances/resolver.spec.ts
@@ -9,30 +9,38 @@ import test from "ava";
 //   },
 // };
 
-test("`TransactionDetail` has amount and account.", t => {
-  t.pass();
-});
+// test("`TransactionDetail` has amount and account.", t => {
+//   t.pass();
+// });
 
-test("`ScheduledTransaction` has start and end date.", t => {
-  t.pass();
-});
+// test("`ScheduledTransaction` has start and end date.", t => {
+//   t.pass();
+// });
 
-test("`ScheduledTransaction` has required meta data.", t => {
-  t.pass();
-});
+// test("`ScheduledTransaction` has required meta data.", t => {
+//   t.pass();
+// });
 
-test("`Transaction` has payment details.", t => {
-  t.pass();
-});
+// test("`Transaction` has payment details.", t => {
+//   t.pass();
+// });
 
-test("`FinancialAccount` is a valid account for contributions.", t => {
-  t.pass();
-});
+// test("`FinancialAccount` is a valid account for contributions.", t => {
+//   t.pass();
+// });
 
-test("`PaymentDetail` has detailed information about a single payment.", t => {
-  t.pass();
-});
+// test("`PaymentDetail` has detailed information about a single payment.", t => {
+//   t.pass();
+// });
 
-test("`SavedPayment` has the required information to repeat transactions.", t => {
-  t.pass();
-});
+// test("`SavedPayment` has the required information to repeat transactions.", t => {
+//   t.pass();
+// });
+
+test.plan("`TransactionDetail` has amount and account.");
+test.plan("`ScheduledTransaction` has start and end date.");
+test.plan("`ScheduledTransaction` has required meta data.");
+test.plan("`Transaction` has payment details.");
+test.plan("`FinancialAccount` is a valid account for contributions.");
+test.plan("`PaymentDetail` has detailed information about a single payment.");
+test.plan("`SavedPayment` has the required information to repeat transactions.");
