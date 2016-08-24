34c2d759600b9c587002e0832ae162ec43f07542
John Pinkerton <jpinkerton88@gmail.com>
2016-08-24 16:53:13 -0400

    more ee content resolver tests

--- test/expression-engine/models/content/resolver.spec.ts
+++ test/expression-engine/models/content/resolver.spec.ts
@@ -1,6 +1,7 @@
 
 import test from "ava";
 import { difference } from "lodash";
+import { createGlobalId } from "../../../../src/util";
 import Resolver from "../../../../src/expression-engine/models/content/resolver";
 
 const sampleData = {
@@ -316,6 +317,200 @@ test("`ContentScripture` returns the passage", t => {
 });
 
 // XXX test all of this resolver
+
+test("`ContentData` should call cleanMarkup with body", t => {
+  const { ContentData } = Resolver;
+  const mockData = {
+    body: "test",
+    legacy_body: null,
+  };
+  const models = {
+    Content: {
+      cleanMarkup: (markup) => {
+        t.is(markup, mockData.body);
+      },
+    },
+  };
+
+  ContentData.body(mockData, {}, { models });
+});
+
+test("`ContentData` should call cleanMarkup with legacy_body if no body", t => {
+  const { ContentData } = Resolver;
+  const mockData = {
+    body: null,
+    legacy_body: "test",
+  };
+  const models = {
+    Content: {
+      cleanMarkup: (markup) => {
+        t.is(markup, mockData.legacy_body);
+      },
+    },
+  };
+
+  ContentData.body(mockData, {}, { models });
+});
+
+test("`ContentData` should return description", t => {
+  const { ContentData } = Resolver;
+  const mockData = {
+    description: "description",
+  };
+
+  const description = ContentData.description(mockData);
+  t.is(description, mockData.description);
+});
+
+test("`ContentData` should return ooyalaId", t => {
+  const { ContentData } = Resolver;
+  const mockData = {
+    video: "id",
+  };
+
+  const ooyalaId = ContentData.ooyalaId(mockData);
+  t.is(ooyalaId, mockData.video);
+});
+
+test("`ContentData` should call splitByNewLines", t => {
+  const { ContentData } = Resolver;
+  const mockData = {
+    tags: "tags",
+  };
+  const models = {
+    Content: {
+      splitByNewLines: (tags) => {
+        t.is(tags, mockData.tags);
+      },
+    },
+  };
+
+  ContentData.tags(mockData, {}, { models });
+});
+
+test("`ContentData` should return speaker", t => {
+  const { ContentData } = Resolver;
+  const mockData = {
+    speaker: "speaker",
+  };
+
+  const speaker = ContentData.speaker(mockData);
+  t.is(speaker, mockData.speaker);
+});
+
+test("`ContentData` should return hashtag", t => {
+  const { ContentData } = Resolver;
+  const mockData = {
+    hashtag: "hashtag",
+  };
+
+  const hashtag = ContentData.hashtag(mockData);
+  t.is(hashtag, mockData.hashtag);
+});
+
+test("`ContentData` should return foreground_color not dark", t => {
+  const { ContentData } = Resolver;
+  const mockData = {
+    lightswitch: null,
+    foreground_color: "dark",
+  };
+
+  const isLight = ContentData.isLight(mockData);
+  t.is(isLight, mockData.foreground_color !== "dark");
+});
+
+test("`ContentData` should return lightswitch not dark for isLight if no foreground_color", t => {
+  const { ContentData } = Resolver;
+  const mockData = {
+    lightswitch: "dark",
+    foreground_color: null,
+  };
+
+  const isLight = ContentData.isLight(mockData);
+  t.is(isLight, mockData.lightswitch !== "dark");
+});
+
+test("`ContentData` should return blank array if no scripture", t => {
+  const { ContentData } = Resolver;
+  const mockData = {
+    entry_id: "1",
+    scripture: null,
+    exp_channel: {},
+  };
+  const models = {};
+
+  const scripture = ContentData.scripture(mockData, {}, { models });
+  t.deepEqual(scripture, []);
+});
+
+test("`ContentData` should call getContentFromMatrix with position if scripture", t => {
+  const { ContentData } = Resolver;
+  const mockData = {
+    entry_id: "1",
+    scripture: "scripture",
+    exp_channel: {
+      exp_channel_fields: {
+        scripture: "field_id_345",
+      },
+    },
+  };
+  const models = {
+    Content: {
+      getContentFromMatrix: (entry_id, scripture, position) => {
+        t.is(entry_id, mockData.entry_id);
+        t.is(scripture, mockData.scripture);
+        t.is(position, 345);
+      },
+    },
+  };
+
+  ContentData.scripture(mockData, {}, { models });
+});
+
+test("`ContentData` should return blank array if no bgcolor and no color", t => {
+  const { ContentData } = Resolver;
+  const mockData = {
+    bgcolor: null,
+    color: null,
+  };
+
+  const colors = ContentData.colors(mockData);
+  t.deepEqual(colors, []);
+});
+
+test("`ContentData` should return color and primary if color", t => {
+  const { ContentData } = Resolver;
+  const mockData = {
+    bgcolor: null,
+    color: "green",
+  };
+
+  const colors = ContentData.colors(mockData);
+  t.deepEqual(
+    colors,
+    [{
+      value: mockData.color,
+      description: "primary",
+    }]
+  );
+});
+
+test("`ContentData` should return bgcolor and primary if no color", t => {
+  const { ContentData } = Resolver;
+  const mockData = {
+    bgcolor: "red",
+    color: null,
+  };
+
+  const colors = ContentData.colors(mockData);
+  t.deepEqual(
+    colors,
+    [{
+      value: mockData.bgcolor,
+      description: "primary",
+    }]
+  );
+});
 
 test("`ContentData` returns blank array if no audio or tracks", async (t) => {
   const { ContentData } = Resolver;
@@ -673,4 +868,246 @@ test("`ContentData` returns only ratio specified", async (t) => {
   result.map((image) => {
     t.is(image.fileLabel, "2:1");
   });
+});
+
+test("`ContentMeta` should exist", t => {
+  const { ContentMeta } = Resolver;
+  t.truthy(ContentMeta);
+});
+
+test("`ContentMeta` should return global id of site_id", t => {
+  const { ContentMeta } = Resolver;
+  const mockData = {
+    site_id: "1",
+  };
+
+  const site = ContentMeta.site(mockData);
+  t.is(site, createGlobalId(mockData.site_id, "Sites"));
+});
+
+test("`ContentMeta` should return global id of channel_id", t => {
+  const { ContentMeta } = Resolver;
+  const mockData = {
+    channel_id: "2",
+  };
+
+  const channel = ContentMeta.channel(mockData);
+  t.is(channel, createGlobalId(mockData.channel_id, "Channel"));
+});
+
+test("`ContentMeta` should return global id of series_id", t => {
+  const { ContentMeta } = Resolver;
+  const mockData = {
+    series_id: "3",
+  };
+  const parentType = {
+    name: "suchparent",
+  };
+
+  const series = ContentMeta.series(mockData, {}, {}, { parentType });
+  t.is(series, createGlobalId(mockData.series_id, parentType.name));
+});
+
+test("`ContentMeta` should return url for urlTitle", t => {
+  const { ContentMeta } = Resolver;
+  const mockData = {
+    url: "url",
+    exp_channel_title: {
+      url_title: "title",
+    },
+  };
+
+  const urlTitle = ContentMeta.urlTitle(mockData);
+  t.is(urlTitle, mockData.url);
+});
+
+test("`ContentMeta` should return false if no url and no exp_channel_title", t => {
+  const { ContentMeta } = Resolver;
+  const mockData = {
+    url: null,
+    exp_channel_title: null,
+  };
+
+  const urlTitle = ContentMeta.urlTitle(mockData);
+  t.falsy(urlTitle);
+});
+
+test("`ContentMeta` should return false if no url and no url_title", t => {
+  const { ContentMeta } = Resolver;
+  const mockData = {
+    url: null,
+    exp_channel_title: {
+      url_title: null,
+    },
+  };
+
+  const urlTitle = ContentMeta.urlTitle(mockData);
+  t.falsy(urlTitle);
+});
+
+test("`ContentMeta` should return url_title if no url", t => {
+  const { ContentMeta } = Resolver;
+  const mockData = {
+    url: null,
+    exp_channel_title: {
+      url_title: "meow",
+    },
+  };
+
+  const urlTitle = ContentMeta.urlTitle(mockData);
+  t.is(urlTitle, mockData.exp_channel_title.url_title);
+});
+
+test("`ContentMeta` should return summary if summary", async (t) => {
+  const { ContentMeta } = Resolver;
+  const mockData = {
+    summary: "summary",
+    body: null,
+    legacy_body: null,
+  };
+  const models = {};
+
+  const summary = await ContentMeta.summary(mockData, {}, { models });
+  t.is(summary, mockData.summary);
+});
+
+test("`ContentMeta` should call cleanMarkup with body if no summary", async (t) => {
+  const { ContentMeta } = Resolver;
+  const mockData = {
+    summary: null,
+    body: "body",
+    legacy_body: "legacy_body",
+  };
+  const models = {
+    Content: {
+      cleanMarkup: (markup) => {
+        t.is(markup, mockData.body);
+      },
+    },
+  };
+
+  await ContentMeta.summary(mockData, {}, { models });
+});
+
+test("`ContentMeta` should call cleanMarkup with legacy_body if no summary or body", async (t) => {
+  const { ContentMeta } = Resolver;
+  const mockData = {
+    summary: null,
+    body: null,
+    legacy_body: "legacy_body",
+  };
+  const models = {
+    Content: {
+      cleanMarkup: (markup) => {
+        t.is(markup, mockData.legacy_body);
+      },
+    },
+  };
+
+  await ContentMeta.summary(mockData, {}, { models });
+});
+
+test("`ContentMeta` should null if no body, legacy_body, or summary", async (t) => {
+  const { ContentMeta } = Resolver;
+  const mockData = {
+    summary: null,
+    body: null,
+    legacy_body: null,
+  };
+  const models = {
+    Content: {
+      cleanMarkup: (markup) => {
+        return null;
+      },
+    },
+  };
+
+  const summary = await ContentMeta.summary(mockData, {}, { models });
+  t.is(summary, null);
+});
+
+test("`ContentMeta` date should call getData on model", t => {
+  const { ContentMeta } = Resolver;
+  const mockData = {
+    exp_channel_title: {
+      day: "day",
+      month: "month",
+      year: "year",
+    },
+  };
+  const models = {
+    Content: {
+      getDate: (day, month, year) => {
+        t.is(day, mockData.exp_channel_title.day);
+        t.is(month, mockData.exp_channel_title.month);
+        t.is(year, mockData.exp_channel_title.year);
+      },
+    },
+  };
+
+  ContentMeta.date(mockData, {}, { models });
+});
+
+test("`ContentMeta` actualDate should call getDateFromUnix", t => {
+  const { ContentMeta } = Resolver;
+  const mockData = {
+    actualdate: "date",
+  };
+  const models = {
+    Content: {
+      getDateFromUnix: (actualDate) => {
+        t.is(actualDate, mockData.actualdate);
+      },
+    },
+  };
+
+  ContentMeta.actualDate(mockData, {}, { models });
+});
+
+test("`ContentMeta` entryDate should call getDateFromUnix", t => {
+  const { ContentMeta } = Resolver;
+  const mockData = {
+    entrydate: "date",
+  };
+  const models = {
+    Content: {
+      getDateFromUnix: (entryDate) => {
+        t.is(entryDate, mockData.entrydate);
+      },
+    },
+  };
+
+  ContentMeta.entryDate(mockData, {}, { models });
+});
+
+test("`ContentMeta` startDate should call getDateFromUnix", t => {
+  const { ContentMeta } = Resolver;
+  const mockData = {
+    startdate: "date",
+  };
+  const models = {
+    Content: {
+      getDateFromUnix: (startDate) => {
+        t.is(startDate, mockData.startdate);
+      },
+    },
+  };
+
+  ContentMeta.startDate(mockData, {}, { models });
+});
+
+test("`ContentMeta` endDate should call getDateFromUnix", t => {
+  const { ContentMeta } = Resolver;
+  const mockData = {
+    enddate: "date",
+  };
+  const models = {
+    Content: {
+      getDateFromUnix: (endDate) => {
+        t.is(endDate, mockData.enddate);
+      },
+    },
+  };
+
+  ContentMeta.endDate(mockData, {}, { models });
 });
