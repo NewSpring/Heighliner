711fd1bbdf7e412f7eac8213896bca9386965159
Jake Dawkins <dawkinsjh@gmail.com>
2016-11-30 16:50:59 -0500

    now pulling saved accounts and transacations.

--- src/rock/models/feeds/queries.js
+++ src/rock/models/feeds/queries.js
@@ -1,4 +1,5 @@
 
+
 export default [
 
   "userFeed(filters: [String]!): [Node]",
--- src/rock/models/feeds/resolver.js
+++ src/rock/models/feeds/resolver.js
@@ -1,23 +1,44 @@
 
-// import { createGlobalId } from "../../../util";
+const resolverMap = {
+  Node: {
+    __resolveType(root, context, info) {
+      if (root.schedule) {
+        return info.schema.getType("Transaction");
+      }
 
-// const MutationReponseResolver = {
-//   error: ({ error }) => error,
-//   success: ({ success, error }) => success || !error,
-//   code: ({ code }) => code,
-// };
+      if (root.guid && root.name) {
+        return info.schema.getType("SavedPayment");
+      }
+
+      return null;
+    },
+  },
+};
 
 export default {
 
   Query: {
     userFeed: (_, { filters }, { models, person }) => {
       if (!person) return null;
-      if (filters.includes("GIVING_DASHBOARD")){
-        return models.Transaction.findByPersonAlias(
-          person.aliases, _, _,
-        );
+
+      if (filters.includes("GIVING_DASHBOARD")) {
+        return Promise.all([
+          models.Transaction.findByPersonAlias(
+            person.aliases, { limit: 3, offset: 0 }, { cache: null },
+          ),
+          models.SavedPayment.findByPersonAlias(
+            person.aliases, { limit: 3, offset: 0 }, { cache: null },
+          ),
+        ])
+          .then(([txns, payments]) => [...txns, ...payments]
+            .map((x) => {
+              if (x.Name) x.__type = "SavedPayment";
+              else x.__type = "Transaction";
+              return x;
+            }));
       }
+
+      return null;
     },
   },
-
 };
