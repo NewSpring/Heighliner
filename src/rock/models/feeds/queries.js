e61b5f1ee42b686e1a4dab6c27b30f3b21491737
James Baxley <james.baxley@newspring.cc>
2016-12-19 02:05:25 -0500

    add home feed to dynamic user feeds

--- src/rock/models/feeds/queries.js
+++ src/rock/models/feeds/queries.js
@@ -2,6 +2,13 @@
 
 export default [
 
-  "userFeed(filters: [String]!): [Node]",
+  `userFeed(
+    filters: [String]!
+    options: String = "{}"
+    limit: Int = 20,
+    skip: Int = 0,
+    status: String = "open",
+    cache: Boolean = true
+  ): [Node]`,
 
 ];
