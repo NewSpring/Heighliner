6c1533f25a57c17f069fa817244cbdf6577d8b0b
Jake Dawkins <dawkinsjh@gmail.com>
2016-12-21 12:14:05 -0500

    added resolver tests

--- src/rock/models/finances/__tests__/resolver.js
+++ src/rock/models/finances/__tests__/resolver.js
@@ -1,5 +1,9 @@
 import resolver from "../resolver";
 
+import render from "../util/statement";
+
+jest.mock("../util/statement");
+
 describe("Query", () => {
   describe("SavedPayment", () => {
     it("finds by person alias", () => {
@@ -372,20 +376,44 @@ describe("Mutation", () => {
   });
 
   describe("transactionStatement", () => {
-    xit("returns a user's transaction statement information", async () => {
+    it("properly calls the util to render a user's statement", async () => {
+      render.mockImplementation((data) => Promise.resolve(data));
+      const person = { GivingGroupId: 1, Id: 1 };
       const models = {
         Transaction: {
-          getStatement: jest.fn(Promise.resolve()),
+          getStatement: jest.fn(() => Promise.resolve({total: 0, transactions: []})),
         },
         Person: {
-          getHomesFromId: jest.fn(Promise.resolve([1,2])),
+          getHomesFromId: jest.fn(() => Promise.resolve([1,2])),
+        }
+      };
+      await resolver.Mutation.transactionStatement(
+        null, { people: [1,2], start: "", end: "" }, { models, person }
+      )
+
+      expect(models.Transaction.getStatement).toBeCalledWith({ start: "", end: "", "givingGroupId": 1, "people": [1, 2] });
+      expect(models.Person.getHomesFromId).toBeCalledWith(1);
+      expect(render).toBeCalledWith({home: 1, person: person, total: 0, transactions: []});
+    });
+    it("gracefully handles errors", async () => {
+      render.mockImplementation((data) => Promise.resolve(data));
+      const person = { GivingGroupId: 1, Id: 1 };
+      const models = {
+        Transaction: {
+          getStatement: jest.fn(() => Promise.reject(new Error("force an error"))),
+        },
+        Person: {
+          getHomesFromId: jest.fn(() => Promise.reject(new Error("force an error"))),
         }
       };
       const result = await resolver.Mutation.transactionStatement(
-        null, { people: [1,2], start: "", end: "" }, { models }
+        null, { people: [1,2], start: "", end: "" }, { models, person }
       );
+
       expect(models.Transaction.getStatement).toBeCalled();
       expect(models.Person.getHomesFromId).toBeCalled();
+      expect(render).toBeCalledWith({home: 1, person: person, total: 0, transactions: []});
+      expect(result).toEqual({ code: 500, error: "force an error", success: false });
     });
   });
 });
