97359e9299fdf3897a3b8bc7a6e282b34c7ca3e0
Jake Dawkins <dawkinsjh@gmail.com>
2017-01-19 10:34:44 -0500

    finished model tests

--- src/rock/models/likes/__tests__/model.js
+++ src/rock/models/likes/__tests__/model.js
@@ -8,39 +8,22 @@ import { MongoConnector } from "../../../../apollos/mongo";
 import { createGlobalId } from "../../../../util/node/model";
 
 jest.mock("node-uuid", () => ({
-  uuid: {
-    v4: () => "1234567"
-  }
+  v4: jest.fn(() => "12345"),
 }));
 
 jest.mock("../../../../apollos/mongo", () => {
-
-  function MongoConnector(name, schema) {
-
-  }
-
+  function MongoConnector(name, schema) {}
   MongoConnector.prototype.find = jest.fn();
   MongoConnector.prototype.remove = jest.fn();
   MongoConnector.prototype.create = jest.fn();
-
-  return {MongoConnector};
+  MongoConnector.prototype.findOne = jest.fn();
+  return { MongoConnector };
 });
-  // MongoConnector: jest.fn(() => ({
-  //   find: () => jest.fn(),
-  //   remove: () => "123",
-  //   create: () => "123",
-  // }))
-  // MongoConnector: ({
-  //   find: () => jest.fn(),
-  //   remove: () => "123",
-  //   create: () => "123",
-  // })
-// }));
 
 jest.mock("../../../../util/cache", () => ({
   defaultCache: {
-    get: jest.fn(() => "123"),
-    del: jest.fn(() => "123")
+    get: jest.fn(),
+    del: jest.fn()
   }
 }));
 
@@ -48,15 +31,28 @@ jest.mock("../../../../util/node/model", () => ({
   createGlobalId: jest.fn(() => "12341234")
 }));
 
+const mockData = {
+  nodeModel: {
+    get: jest.fn(),
+  },
+};
+
 describe("Like", () => {
   let like;
+  let cache;
+  const mongo = MongoConnector.prototype;
 
   beforeEach(() => {
     like = new Like();
+    cache = defaultCache;
   });
 
   afterEach(() => {
     like = null;
+    mongo.find.mockReset();
+    mongo.remove.mockReset();
+    mongo.create.mockReset();
+    mongo.findOne.mockReset();
   })
 
   describe("getFromUserId", () => {
@@ -66,13 +62,77 @@ describe("Like", () => {
     });
     it("should do a cache lookup", () => {
       like.getFromUserId("1234");
-      expect(defaultCache.get.mock.calls[1][0]).toEqual("12341234");
+      expect(cache.get).toBeCalled();
+      expect(cache.get.mock.calls[1][0]).toEqual("12341234");
     });
     it("should do a mongo find on cache miss", () => {
       defaultCache.get.mockImplementationOnce((a, b) => b());
       like.getFromUserId("1234");
-      console.log(MongoConnector().find());
-      expect(MongoConnector().find()).toBeCalled();
+      expect(mongo.find).toBeCalledWith({ userId: "1234" });
+    });
+  });
+
+  describe("getLikedContent", () => {
+    beforeEach(() => {
+      like.getFromUserId = jest.fn(() => [{entryId: "harambe"}]);
+    });
+    it("should call getFromUserId", async () => {
+      await like.getLikedContent("1234", mockData.nodeModel);
+      expect(like.getFromUserId).toBeCalledWith("1234");
+      mockData.nodeModel.get.mockReset();
+    });
+    it("should call get on node model with entry ids", async () => {
+      await like.getLikedContent("1234", mockData.nodeModel);
+      expect(mockData.nodeModel.get).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe("toggleLike", () => {
+    beforeEach(() => {
+      like.getLikedContent = jest.fn();
+    });
+
+    it("should lookup existing like", () => {
+      //default state: no current like
+      mongo.findOne.mockReturnValueOnce(null);
+
+      like.toggleLike("abcde", "1234", mockData.nodeModel);
+      expect(mongo.findOne).toBeCalledWith({ "entryId": "abcde", "userId": "1234" });
+    });
+    it("should remove current like", async () => {
+      // mongo needs to find an existing like
+      mongo.findOne.mockReturnValueOnce({_id: "harambe"});
+
+      await like.toggleLike("abcde", "1234", mockData.nodeModel);
+      expect(mongo.remove).toBeCalledWith({ _id: "harambe" });
+    });
+    it("should create a like if one isn't found", async () => {
+      //default state: no current like
+      mongo.findOne.mockReturnValueOnce(null);
+      Date = jest.fn();
+
+      await like.toggleLike("abcde", "1234", mockData.nodeModel);
+      expect(mongo.create).toBeCalledWith({
+        _id: "12345",
+        createdAt: {},
+        entryId: "abcde",
+        userId: "1234",
+      });
+    });
+    it("deletes cache", async () => {
+      createGlobalId.mockReset();
+      createGlobalId.mockReturnValue("123456789");
+      cache.del.mockReset();
+
+      await like.toggleLike("abcde", "1234", mockData.nodeModel);
+
+      expect(createGlobalId).toBeCalledWith("1234");
+      expect(cache.del).toBeCalledWith("123456789");
+    });
+    it("returns list of likes", async () => {
+      like.getLikedContent.mockReturnValueOnce([{}, {}]);
+      const likes = await like.toggleLike("abcde", "1234", mockData.nodeModel);
+      expect(Array.isArray(likes)).toBe(true);
     });
   });
 
