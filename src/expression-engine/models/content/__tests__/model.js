0a263c934bda118e2f9f8d8d42bce05b38d6d780
Jake Dawkins <dawkinsjh@gmail.com>
2017-02-27 14:25:59 -0500

    added tests for new model method on content

--- src/expression-engine/models/content/__tests__/model.js
+++ src/expression-engine/models/content/__tests__/model.js
@@ -1,6 +1,18 @@
 
 import { Content } from "../model";
 import { createGlobalId } from "../../../../util/node/model";
+import { ChannelData } from "../tables";
+
+jest.mock("../tables", () => ({
+  ChannelData: {
+    find: jest.fn(),
+  },
+  channelDataSchema: {},
+  channelSchema: {},
+  channelTitleSchema: {},
+  Channels: { Model: "wow" },
+  ChannelTitles: { Model: "lol" },
+}));
 
 jest.mock("../../../../util/node/model");
 
@@ -43,6 +55,104 @@ describe("findByUrlTitle", () => {
     Model.cache.get = jest.fn(() => ({}));
     const res = await Model.findByUrlTitle("articles","harambe");
     expect(res).toEqual(null);
+  });
+
+});
+
+describe("findByCampusName", () => {
+  let Model;
+  beforeEach(() => {
+    Model = new Content();
+    Model.cache.encode = jest.fn(() => "12345");
+    Model.cache.get = jest.fn();
+  });
+  afterEach(() => {
+    Model = null;
+    ChannelData.find.mockReset();
+  });
+
+  it("should exist", () => {
+    expect(Model.findByCampusName).toBeTruthy();
+  });
+
+  it("should call cache lookup", async () => {
+    Model.getFromPublishedIds = jest.fn(() => ["dat Haramboi"]);
+    Model.cache.get = jest.fn(() => Promise.resolve({ entry_id: "1123"}));
+    const res = await Model.findByCampusName({}, "Cincinnati Zoo", true, null);
+    expect(Model.cache.get).toBeCalled();
+    expect(Model.cache.get.mock.calls[0][0]).toEqual("12345");
+    expect(typeof Model.cache.get.mock.calls[0][1]).toEqual("function");
+    expect(Model.cache.get.mock.calls[0][2]).toEqual({ ttl: 3600, cache: false });
+  });
+
+  it("should call find with proper where value for logged out users", async () => {
+    Model.getFromPublishedIds = jest.fn(() => ["dat Haramboi"]);
+    ChannelData.find.mockReturnValueOnce(Promise.resolve([]));
+    //force cache to call second param (find)
+    Model.cache.get.mockImplementationOnce((a, b) => b());
+    const res = await Model.findByCampusName({}, null, true, null);
+    expect(ChannelData.find.mock.calls[0][0].where.field_id_651)
+      .toEqual({ $or: ["", null] });
+  });
+
+  it("should call find with proper where value for logged in users", async () => {
+    Model.getFromPublishedIds = jest.fn(() => ["dat Haramboi"]);
+    ChannelData.find.mockReturnValueOnce(Promise.resolve([]));
+    //force cache to call second param (find)
+    Model.cache.get.mockImplementationOnce((a, b) => b());
+    const res = await Model.findByCampusName({}, "Palace de Harambe", true, null);
+    expect(ChannelData.find.mock.calls[0][0].where.field_id_651)
+      .toEqual({ $or: [{ $like: `%Palace de Harambe`}, "", null ]});
+  });
+
+  it("should pass correct value for globals to find", async () => {
+    Model.getFromPublishedIds = jest.fn(() => ["dat Haramboi"]);
+    ChannelData.find.mockReturnValueOnce(Promise.resolve([]));
+    //force cache to call second param (find)
+    Model.cache.get.mockImplementationOnce((a, b) => b());
+    const res = await Model.findByCampusName({}, "Palace de Harambe", false, null);
+    expect(ChannelData.find.mock.calls[0][0].where.field_id_651)
+      .toEqual({ $like: `%Palace de Harambe`});
+  });
+
+  it("should pass correct value for channel", async () => {
+    Model.getFromPublishedIds = jest.fn(() => ["dat Haramboi"]);
+    ChannelData.find.mockReturnValueOnce(Promise.resolve([]));
+    //force cache to call second param (find)
+    Model.cache.get.mockImplementationOnce((a, b) => b());
+    const res = await Model.findByCampusName({}, "Palace de Harambe", false, null);
+    expect(ChannelData.find.mock.calls[0][0].include[0].where)
+      .toEqual({});
+  });
+
+  it("should pass correct value for channelTitles", async () => {
+    Model.getFromPublishedIds = jest.fn(() => ["dat Haramboi"]);
+    ChannelData.find.mockReturnValueOnce(Promise.resolve([]));
+    //force cache to call second param (find)
+    Model.cache.get.mockImplementationOnce((a, b) => b());
+    const res = await Model.findByCampusName({}, "Palace de Harambe", false, null);
+    expect(ChannelData.find.mock.calls[0][0].include[1].where)
+      .toMatchSnapshot();
+  });
+
+  it("should pass limit", async () => {
+    Model.getFromPublishedIds = jest.fn(() => ["dat Haramboi"]);
+    ChannelData.find.mockReturnValueOnce(Promise.resolve([]));
+    //force cache to call second param (find)
+    Model.cache.get.mockImplementationOnce((a, b) => b());
+    const res = await Model.findByCampusName({ limit: 5, offset: 10 }, "Palace de Harambe", false, null);
+    expect(ChannelData.find.mock.calls[0][0].limit)
+      .toEqual(5);
+  });
+
+  it("should pass offset", async () => {
+    Model.getFromPublishedIds = jest.fn(() => ["dat Haramboi"]);
+    ChannelData.find.mockReturnValueOnce(Promise.resolve([]));
+    //force cache to call second param (find)
+    Model.cache.get.mockImplementationOnce((a, b) => b());
+    const res = await Model.findByCampusName({ limit: 5, offset: 10 }, "Palace de Harambe", false, null);
+    expect(ChannelData.find.mock.calls[0][0].offset)
+      .toEqual(10);
   });
 
 });
--- src/rock/models/feeds/resolver.js
+++ src/rock/models/feeds/resolver.js
@@ -1,19 +1,6 @@
 
 import { flatten } from "lodash";
 
-// for filtering news. allows logged out users to see gloabl news only,
-// and logged in users to see global and their campus
-/*
-  userCampus: { Guid: "" }
-  entryCampus: { Guid: "" }
-  => truthy/falsy
-*/
-export const shouldShowEntry = (userCampus, entryCampus) => {
-  return !userCampus
-    ? !entryCampus
-    : Boolean(!entryCampus || (userCampus.Guid === entryCampus.Guid))
-}
-
 export default {
 
   Query: {
@@ -45,30 +32,10 @@ export default {
           ? await models.Person.getCampusFromId(person.Id, { cache })
           : null;
 
-        // TODO: filter other campuses by lookup query here
-        let content = await models.Content.find({
+        let content = await models.Content.findByCampusName({
           channel_name: { $or: channels }, offset: skip, limit, status,
-        }, cache);
+        }, userCampus ? userCampus.Name : null, true);
 
-        // logged out only see global news
-        // logged in only see news that is global, or their campus
-        if (showsNews) {
-          /*
-            XXX since you can't filter on an async filter function, I have to iterate over
-            the array of entries and add a shouldBeShown key than I can use to filter on after
-          */
-          for (let i=0; i < content.length; i++) {
-            const entryCampus = await content[i].campus
-              ? await models.Campus.find({ Name: content[i].campus.split(" ")[2] }).then(x => x.shift())
-              : null;
-            content[i] = { ...content[i], shouldBeShown: shouldShowEntry(userCampus, entryCampus) };
-          }
-
-          content = content.filter( x => x.shouldBeShown );
-
-        }
-
-        // add filtered items to the list
         filterQueries.push(content);
       }
 
