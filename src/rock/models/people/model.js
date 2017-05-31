edf8f28e2f94a8b84f5c03867ba9a4bdc7609cb8
delianides <github@delianides.com>
2017-05-31 10:30:21 -0400

    added IsActive to getGroups on person modal

--- src/rock/models/people/model.js
+++ src/rock/models/people/model.js
@@ -246,8 +246,8 @@ export class Person extends Rock {
   async getGroups(personId, groupTypes) {
     const groupTypeIds = !Array.isArray(groupTypes) ? [groupTypes] : groupTypes;
     const where = groupTypeIds.length
-      ? { GroupTypeId: { $or: groupTypeIds } }
-      : null;
+      ? { GroupTypeId: { $or: groupTypeIds }, IsActive: true }
+      : { IsActive: true };
 
     return this.cache.get(
       `${personId}:GroupMemberId:${groupTypeIds}:GroupTypes`,
