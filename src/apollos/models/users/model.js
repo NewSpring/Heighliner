5c42f60f5a7c2d76460ff4d6a140132e2b9b2593
Marcelo Reyna <Lepozepo@gmail.com>
2017-11-21 13:52:26 -0800

    Integrate with API

--- src/apollos/models/users/model.js
+++ src/apollos/models/users/model.js
@@ -2,7 +2,7 @@ import moment from "moment";
 import stripTags from "striptags";
 import makeNewGuid from "./makeNewGuid";
 import sendEmail from "./sendEmail";
-import api from "./rockAPI";
+import api from "./api";
 
 export class User {
   async getByBasicAuth(userPasswordString = "") {
@@ -16,7 +16,7 @@ export class User {
       const isAuthorized = await this.checkUserCredentials(username, password);
       if (!isAuthorized) throw new Error("User not authorized");
 
-      const [user] = await api.get(`UserLogins?$filter=UserName eq '${username}'`);
+      const [user] = await api.get(`/UserLogins?$filter=UserName eq '${username}'`);
       return user;
     } catch (err) {
       throw err;
@@ -25,9 +25,11 @@ export class User {
 
   async checkUserCredentials(Username, Password) {
     try {
-      const isAuthorized = await api.post("Auth/login", {
-        Username,
-        Password,
+      const isAuthorized = await api.post("/Auth/login", {
+        data: {
+          Username,
+          Password,
+        },
       });
       return isAuthorized && !isAuthorized.statusText;
     } catch (err) {
@@ -38,13 +40,17 @@ export class User {
   async loginUser(user = {}) {
     try {
       if (!user.IsConfirmed) {
-        api.post(`UserLogins/${user.Id}`, {
-          IsConfirmed: true,
+        api.post(`/UserLogins/${user.Id}`, {
+          data: {
+            IsConfirmed: true,
+          },
         });
       }
 
-      api.patch(`UserLogins/${user.Id}`, {
-        LastLoginDateTime: `${moment().toISOString()}`,
+      api.patch(`/UserLogins/${user.Id}`, {
+        data: {
+          LastLoginDateTime: `${moment().toISOString()}`,
+        },
       });
 
       return user;
@@ -60,16 +66,18 @@ export class User {
       lastName,
     } = props;
 
-    return api.post("People", {
-      Email: email,
-      Guid: makeNewGuid(),
-      FirstName: stripTags(firstName),
-      LastName: stripTags(lastName),
-      IsSystem: false,
-      Gender: 0,
-      RecordTypeValueId: 1,
-      ConnectionStatusValueId: 67, // Web Prospect
-      SystemNote: "Created from NewSpring Apollos",
+    return api.post("/People", {
+      data: {
+        Email: email,
+        Guid: makeNewGuid(),
+        FirstName: stripTags(firstName),
+        LastName: stripTags(lastName),
+        IsSystem: false,
+        Gender: 0,
+        RecordTypeValueId: 1,
+        ConnectionStatusValueId: 67, // Web Prospect
+        SystemNote: "Created from NewSpring Apollos",
+      },
     });
   }
 
@@ -80,13 +88,15 @@ export class User {
       personId,
     } = props;
 
-    return api.post("UserLogins", {
-      PersonId: personId,
-      EntityTypeId: 27,
-      UserName: email,
-      IsConfirmed: true,
-      PlainTextPassword: password,
-      LastLoginDateTime: `${moment().toISOString()}`,
+    return api.post("/UserLogins", {
+      data: {
+        PersonId: personId,
+        EntityTypeId: 27,
+        UserName: email,
+        IsConfirmed: true,
+        PlainTextPassword: password,
+        LastLoginDateTime: `${moment().toISOString()}`,
+      },
     });
   }
 
@@ -111,13 +121,15 @@ export class User {
         personId,
       });
 
-      const user = await api.get(`UserLogins/${userId}`);
-      const person = await api.get(`People/${personId}`);
+      const [user, person] = await Promise.all([
+        api.get(`/UserLogins/${userId}`),
+        api.get(`/People/${personId}`),
+      ]);
 
       // Removed saving it to a constant
       // because we don't know if this ID could change
       // from other sources (migrations, integrity checks, etc)
-      const [systemEmail] = await api.get("SystemEmails?$filter=Title eq 'Account Created'");
+      const [systemEmail] = await api.get("/SystemEmails?$filter=Title eq 'Account Created'");
 
       await sendEmail(
         systemEmail.Id,
