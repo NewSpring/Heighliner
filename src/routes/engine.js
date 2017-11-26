accd2ef6ce65a8d3ec133aff2de6a93d00d3ead2
delianides <github@delianides.com>
2017-11-26 10:12:09 -0500

    updated engine and config to match new api

--- src/routes/engine.js
+++ src/routes/engine.js
@@ -3,9 +3,6 @@ import { Engine } from "apollo-engine";
 const apolloEngine = new Engine({
   engineConfig: {
     apiKey: process.env.ENGINE_API_KEY,
-    logcfg: {
-      level: "debug",
-    },
   },
   graphqlPort: process.env.PORT,
 });
