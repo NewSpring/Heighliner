5bc574ef1debfba270f85a535019e6b4666dd612
Marcelo Reyna <Lepozepo@gmail.com>
2017-12-07 14:25:00 -0800

    Hard-code ROCK_TOKEN_NAME
    It should be consistent across rocks

--- src/apollos/models/users/api.js
+++ src/apollos/models/users/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default axios.create({
   baseURL: process.env.ROCK_URL,
   headers: {
-    [process.env.ROCK_TOKEN_NAME]: process.env.ROCK_TOKEN, // Not too sure about this
+    "Authorization-Token": process.env.ROCK_TOKEN,
     "Content-Type": "application/json",
   },
 });
