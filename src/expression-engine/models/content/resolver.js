b4f93c27cb14913deff6e2f45263f3927801dca9
Conrad VanLandingham <hi@convan.me>
2017-12-12 23:36:48 -0600

    Add video prop to content

--- src/expression-engine/models/content/resolver.js
+++ src/expression-engine/models/content/resolver.js
@@ -103,6 +103,21 @@ export default {
     body: ({ body, legacy_body }, _, { models }) => models.Content.cleanMarkup(body || legacy_body),
     description: ({ description }) => description,
     ooyalaId: ({ video }) => video,
+    video: ({ video }) => {
+      // Todo: should these be stored as ENV vars?
+      const pbid = 'ZmJmNTVlNDk1NjcwYTVkMzAzODkyMjg0&pcode=';
+      const pcode = 'E1dWM6UGncxhent7MRATc3hmkzUD';
+
+      // Todo: this is hosted on a personal dropbox. Should be moved to S3 or something (needs CORs though)
+      const playerConfig = 'https%3A%2F%2Fdl.dropbox.com%2Fs%2Fsodcv1d9a4ezwm4%2Fskin.new.json%3Fdl%3D1';
+
+      const embedUrl = `https://player.ooyala.com/static/v4/production/latest/skin-plugin/iframe.html?ec=${video}&pbid=${pbid}&pcode=${pcode}&skin.config=${playerConfig}`;
+
+      return ({
+        id: video,
+        embedUrl,
+      });
+    },
     tags: ({ tags }, _, { models }) => models.Content.splitByNewLines(tags),
     speaker: ({ speaker }) => speaker,
     hashtag: ({ hashtag }) => hashtag,
