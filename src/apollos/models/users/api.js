34b18fa39b02229cf42a317342dc77f9cb4da2fd
Marcelo Reyna <Lepozepo@gmail.com>
2017-12-15 11:36:06 -0800

    Rebuild API

--- src/apollos/models/users/api.js
+++ src/apollos/models/users/api.js
@@ -1,9 +1,83 @@
-import axios from "axios";
+import fetch from "isomorphic-fetch";
 
-export default axios.create({
-  baseURL: `${process.env.ROCK_URL}api`,
+const baseURL = `${process.env.ROCK_URL}api`;
+const CONFIG = {
   headers: {
     "Authorization-Token": process.env.ROCK_TOKEN,
     "Content-Type": "application/json",
   },
-});
+};
+
+function statusResponseResolver(r = {}) {
+  if (r.status === 204) return true;
+  if (r.status >= 200 && r.status < 300) {
+    return r.json();
+  }
+  throw new Error(r.statusText);
+}
+
+export function get(url, config = {}) {
+  return fetch(`${baseURL}${url}`, {
+    method: "GET",
+    ...CONFIG,
+    ...config,
+  })
+    .then(statusResponseResolver);
+}
+
+export function del(url, config = {}) {
+  return fetch(`${baseURL}${url}`, {
+    method: "DELETE",
+    ...CONFIG,
+    ...config,
+  })
+    .then(statusResponseResolver);
+}
+
+export function head(url, config = {}) {
+  return fetch(`${baseURL}${url}`, {
+    method: "HEAD",
+    ...CONFIG,
+    ...config,
+  })
+    .then(statusResponseResolver);
+}
+
+export function options(url, config = {}) {
+  return fetch(`${baseURL}${url}`, {
+    method: "OPTIONS",
+    ...CONFIG,
+    ...config,
+  })
+    .then(statusResponseResolver);
+}
+
+export function post(url, data = {}, config = {}) {
+  return fetch(`${baseURL}${url}`, {
+    method: "POST",
+    ...CONFIG,
+    body: JSON.stringify(data),
+    ...config,
+  })
+    .then(statusResponseResolver);
+}
+
+export function put(url, data = {}, config = {}) {
+  return fetch(`${baseURL}${url}`, {
+    method: "PUT",
+    ...CONFIG,
+    body: JSON.stringify(data),
+    ...config,
+  })
+    .then(statusResponseResolver);
+}
+
+export function patch(url, data = {}, config = {}) {
+  return fetch(`${baseURL}${url}`, {
+    method: "PATCH",
+    ...CONFIG,
+    body: JSON.stringify(data),
+    ...config,
+  })
+    .then(statusResponseResolver);
+}
