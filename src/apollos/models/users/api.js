6a12a30f4972fdd8a2af729687acaec33e3b231d
Marcelo Reyna <Lepozepo@gmail.com>
2017-12-15 09:14:54 -0800

    Patch baseURL

--- src/apollos/models/users/api.js
+++ src/apollos/models/users/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: process.env.ROCK_URL,
+  baseURL: `${process.env.ROCK_URL}api`,
   headers: {
     "Authorization-Token": process.env.ROCK_TOKEN,
     "Content-Type": "application/json",
