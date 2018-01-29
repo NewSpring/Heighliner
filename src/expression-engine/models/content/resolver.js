fafb55be1d6d0878e6d9cdd7ecc9a9e7e0e11e64
Conrad VanLandingham <hi@convan.me>
2018-01-29 12:58:46 -0600

    disable playerConfig on video for now

--- src/expression-engine/models/content/resolver.js
+++ src/expression-engine/models/content/resolver.js
@@ -116,7 +116,7 @@ export default {
       if (!video) return null;
       const pbid = 'ZmJmNTVlNDk1NjcwYTVkMzAzODkyMjg0';
       const pcode = 'E1dWM6UGncxhent7MRATc3hmkzUD';
-      const playerConfig = 'https%3A%2F%2Fd3n6tjerleuu41.cloudfront.net%2Fnewspring%2Fskin.new.json';
+      const playerConfig = ''; // 'https%3A%2F%2Fd3n6tjerleuu41.cloudfront.net%2Fnewspring%2Fskin.new.json';
 
       const embedUrl = `https://player.ooyala.com/static/v4/production/latest/skin-plugin/iframe.html?ec=${video}&pbid=${pbid}&pcode=${pcode}&skin.config=${playerConfig}`;
 
