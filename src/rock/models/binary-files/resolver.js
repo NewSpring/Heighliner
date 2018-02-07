e2904689bacea624715c3eec9d9df36043b8563c
Marcelo Reyna <Lepozepo@gmail.com>
2018-02-07 11:49:38 -0800

    260a image uploads (#356)
    * Build attachPhotoIdToUser mutation
    
    * Make sure photo resolver returns something even if there is nothing or an error occurs
    
    * Patch test

--- src/rock/models/binary-files/resolver.js
+++ src/rock/models/binary-files/resolver.js
@@ -1,6 +1,18 @@
-
-// import { createGlobalId } from "../../../util";
-
 export default {
-
+  Mutation: {
+    async attachPhotoIdToUser(_, props, { models, user }) {
+      try {
+        // NOTE: context person could be cached
+        // therefore we need to query again
+        const person = await models.User.getUserProfile(user.PersonId);
+        return models.BinaryFile.attachPhotoIdToUser({
+          personId: person.Id,
+          previousPhotoId: person.PhotoId,
+          newPhotoId: props.id,
+        });
+      } catch (err) {
+        throw err;
+      }
+    },
+  },
 };
